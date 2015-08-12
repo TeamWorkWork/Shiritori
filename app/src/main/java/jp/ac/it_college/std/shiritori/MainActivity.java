08d9680ff427f2b4a7b00cecc787102dc290304f
s13012 <s13012@std.it-college.ac.jp>
2015-08-12 09:57:32 +0900

    タイトル画面「START」ラベルのクリックリスナーを実装

--- app/src/main/java/jp/ac/it_college/std/shiritori/MainActivity.java
+++ app/src/main/java/jp/ac/it_college/std/shiritori/MainActivity.java
@@ -2,9 +2,10 @@ package jp.ac.it_college.std.shiritori;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +16,20 @@ public class MainActivity extends Activity {
         TextView startTextView = (TextView) findViewById(R.id.lbl_start);
         Winker winker = new Winker(startTextView);
         winker.startWink();
+
+        // Viewのクリックリスナーをセット
+        startTextView.setOnClickListener(this);
     }
 
+    private void startClicked() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.lbl_start:
+                startClicked();
+        }
+    }
 }

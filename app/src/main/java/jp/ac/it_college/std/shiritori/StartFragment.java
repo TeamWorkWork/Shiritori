562b176f3b1ece8abc024ab6ad64c9e45430fec9
s13012 <s13012@std.it-college.ac.jp>
2015-08-12 11:26:23 +0900

    「START」ラベルクリック時の処理を実装

--- app/src/main/java/jp/ac/it_college/std/shiritori/StartFragment.java
+++ app/src/main/java/jp/ac/it_college/std/shiritori/StartFragment.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class StartFragment extends Fragment {
+public class StartFragment extends Fragment implements View.OnClickListener{
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -22,8 +22,26 @@ public class StartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false);
+        View view = inflater.inflate(R.layout.fragment_start, container, false);
+        view.findViewById(R.id.container_start).setOnClickListener(this);
+
+        return view;
     }
 
+    private void startClicked() {
+        //「START」ラベルが押された場合フラグメントを切り替える
+        getFragmentManager().beginTransaction()
+                .replace(R.id.container_bottom, new ModeSelectFragment())
+                .addToBackStack(null)
+                .commit();
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.container_start:
+                startClicked();
+                break;
+        }
+    }
 }

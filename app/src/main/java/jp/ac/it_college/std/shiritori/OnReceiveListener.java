077e874153957189a0b770247f3e074f7a5bedab
s13012 <s13012@std.it-college.ac.jp>
2015-08-12 13:51:50 +0900

    OnReceiveListenerの実装完了

--- app/src/main/java/jp/ac/it_college/std/shiritori/OnReceiveListener.java
+++ app/src/main/java/jp/ac/it_college/std/shiritori/OnReceiveListener.java
@@ -1,5 +1,10 @@
 package jp.ac.it_college.std.shiritori;
 
-public interface OnReceiveListener {
+import android.content.Intent;
 
+public interface OnReceiveListener {
+    void onStateChanged(Intent intent);
+    void onPeersChanged(Intent intent);
+    void onConnectionChanged(Intent intent);
+    void onDeviceChanged(Intent intent);
 }
--- app/src/main/java/jp/ac/it_college/std/shiritori/WiFiDirectBroadcastReceiver.java
+++ app/src/main/java/jp/ac/it_college/std/shiritori/WiFiDirectBroadcastReceiver.java
@@ -1,4 +1,34 @@
 package jp.ac.it_college.std.shiritori;
 
-public class WiFiDirectBroadcastReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
+    private OnReceiveListener listener;
+
+    public WiFiDirectBroadcastReceiver(OnReceiveListener listener) {
+        super();
+        this.listener = listener;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            // Check to see if Wi-Fi is enabled and notify appropriate activity
+            listener.onStateChanged(intent);
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+            listener.onPeersChanged(intent);
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            // Respond to new connection or disconnections
+            listener.onConnectionChanged(intent);
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            // Respond to this device's wifi state changing
+            listener.onDeviceChanged(intent);
+        }
+    }
 }

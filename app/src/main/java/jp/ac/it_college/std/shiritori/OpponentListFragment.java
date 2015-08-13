d95a15f9e5c3f91d7c08603f08e3e97ecc26fb33
s13012 <s13012@std.it-college.ac.jp>
2015-08-13 11:07:37 +0900

    OpponentListFragmentにonPeersAvailableメソッドを実装

--- app/src/main/java/jp/ac/it_college/std/shiritori/OpponentListFragment.java
+++ app/src/main/java/jp/ac/it_college/std/shiritori/OpponentListFragment.java
@@ -2,12 +2,14 @@ package jp.ac.it_college.std.shiritori;
 
 
 import android.app.ListFragment;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager.PeerListListener;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class OpponentListFragment extends ListFragment {
+public class OpponentListFragment extends ListFragment implements PeerListListener{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -16,4 +18,8 @@ public class OpponentListFragment extends ListFragment {
         return inflater.inflate(R.layout.fragment_opponent_list, container, false);
     }
 
+    @Override
+    public void onPeersAvailable(WifiP2pDeviceList peers) {
+
+    }
 }

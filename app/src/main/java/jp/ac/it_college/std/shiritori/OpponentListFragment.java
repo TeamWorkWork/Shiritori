455c24361ed04b13174abd3609721b826dbd630a
s13012 <s13012@std.it-college.ac.jp>
2015-08-13 11:04:14 +0900

    OpponentListFragmentのスーパークラスをListFragmentに変更

--- app/src/main/java/jp/ac/it_college/std/shiritori/OpponentListFragment.java
+++ app/src/main/java/jp/ac/it_college/std/shiritori/OpponentListFragment.java
@@ -1,13 +1,13 @@
 package jp.ac.it_college.std.shiritori;
 
 
+import android.app.ListFragment;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class OpponentListFragment extends Fragment {
+public class OpponentListFragment extends ListFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,

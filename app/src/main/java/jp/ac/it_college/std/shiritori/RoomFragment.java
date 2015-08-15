b6ba8987839f1dda539d39d974e1ce9cf2d63c4d
Akinori Maesoko <s13012@std.it-college.ac.jp>
2015-08-15 19:23:22 +0900

    ルーム画面に切り替わった時にデバイス名をセットする処理を実装

--- app/src/main/java/jp/ac/it_college/std/shiritori/RoomFragment.java
+++ app/src/main/java/jp/ac/it_college/std/shiritori/RoomFragment.java
@@ -2,12 +2,23 @@ package jp.ac.it_college.std.shiritori;
 
 
 import android.app.ListFragment;
+import android.net.wifi.p2p.WifiP2pDevice;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class RoomFragment extends ListFragment {
+
+    private WifiP2pDevice device;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        this.device = ((MainActivity) getActivity()).getDevice();
+        ((TextView) getView().findViewById(R.id.my_name)).setText(device.deviceName);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,

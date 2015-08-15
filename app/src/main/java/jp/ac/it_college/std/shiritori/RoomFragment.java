fc596d6b43c6f17c2c8b8e690b22aeef11640ebf
Akinori Maesoko <s13012@std.it-college.ac.jp>
2015-08-15 18:35:01 +0900

    RoomFragmentのスーパークラスをListFragmentに変更

--- app/src/main/java/jp/ac/it_college/std/shiritori/RoomFragment.java
+++ app/src/main/java/jp/ac/it_college/std/shiritori/RoomFragment.java
@@ -1,13 +1,13 @@
 package jp.ac.it_college.std.shiritori;
 
 
+import android.app.ListFragment;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class RoomFragment extends Fragment {
+public class RoomFragment extends ListFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -15,6 +15,5 @@ public class RoomFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_room, container, false);
     }
-
 
 }

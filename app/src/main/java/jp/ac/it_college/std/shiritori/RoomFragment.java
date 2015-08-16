64e24154ca7823e09ffb97d98dfaca1fbb25ced2
Akinori Maesoko <s13012@std.it-college.ac.jp>
2015-08-17 00:58:38 +0900

    ルーム画面移動時に周囲のピアを検索する処理を実装

--- app/src/main/java/jp/ac/it_college/std/shiritori/RoomFragment.java
+++ app/src/main/java/jp/ac/it_college/std/shiritori/RoomFragment.java
@@ -3,16 +3,37 @@ package jp.ac.it_college.std.shiritori;
 
 import android.app.ListFragment;
 import android.content.Intent;
+import android.net.NetworkInfo;
 import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pGroup;
+import android.net.wifi.p2p.WifiP2pInfo;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.ConnectionInfoListener;
+import android.net.wifi.p2p.WifiP2pManager.GroupInfoListener;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class RoomFragment extends ListFragment implements OnReceiveListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoomFragment extends ListFragment
+        implements OnReceiveListener, View.OnClickListener, ConnectionInfoListener,
+        GroupInfoListener {
 
     private WifiP2pDevice device;
+    private WifiP2pManager manager;
+    private WifiP2pManager.Channel channel;
+    private WifiP2pInfo info;
+    private List<WifiP2pDevice> peers = new ArrayList<>();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ((MainActivity) getActivity()).getEventManager().addOnReceiveListener(this);
+    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -20,7 +41,14 @@ public class RoomFragment extends ListFragment implements OnReceiveListener {
 
         this.device = ((MainActivity) getActivity()).getDevice();
         ((TextView) getView().findViewById(R.id.my_name)).setText(device.deviceName);
-        ((MainActivity) getActivity()).getEventManager().addOnReceiveListener(this);
+
+        getView().findViewById(R.id.btn_game_start).setOnClickListener(this);
+
+        setListAdapter(new WiFiPeerListAdapter(getActivity(), R.layout.row_devices, peers));
+        manager = ((MainActivity) getActivity()).getManager();
+        channel = ((MainActivity) getActivity()).getChannel();
+
+        discover();
     }
 
     @Override
@@ -31,22 +59,88 @@ public class RoomFragment extends ListFragment implements OnReceiveListener {
     }
 
     @Override
-    public void onStateChanged(Intent intent) {
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_game_start:
+                onClickGameStart();
+                break;
+        }
+    }
 
+    private void onClickGameStart() {
+    }
+
+    private void discover() {
+        if (manager != null) {
+            manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+                @Override
+                public void onSuccess() {
+
+                }
+
+                @Override
+                public void onFailure(int reason) {
+
+                }
+            });
+        }
+    }
+
+    /*
+    Implemented OnReceiveListener
+     */
+    @Override
+    public void onStateChanged(Intent intent) {
     }
 
     @Override
     public void onPeersChanged(Intent intent) {
-
     }
 
     @Override
     public void onConnectionChanged(Intent intent) {
+        if (manager == null) {
+            return;
+        }
 
+        NetworkInfo networkInfo = (NetworkInfo) intent
+                .getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+
+        if (networkInfo.isConnected()) {
+            manager.requestConnectionInfo(channel, this);
+            manager.requestGroupInfo(channel, this);
+        } else {
+            peers.clear();
+            ((WiFiPeerListAdapter) getListAdapter()).notifyDataSetChanged();
+            discover();
+        }
     }
 
     @Override
     public void onDeviceChanged(Intent intent) {
-
     }
+
+    /*
+    Implemented ConnectionInfoListener
+     */
+    @Override
+    public void onConnectionInfoAvailable(WifiP2pInfo info) {
+        this.info = info;
+    }
+
+    /*
+    Implemented GroupInfoListener
+     */
+    @Override
+    public void onGroupInfoAvailable(WifiP2pGroup group) {
+        peers.clear();
+        if (info.isGroupOwner) {
+            peers.addAll(group.getClientList());
+        } else {
+            peers.add(group.getOwner());
+        }
+        ((WiFiPeerListAdapter) getListAdapter()).notifyDataSetChanged();
+    }
+
+
 }

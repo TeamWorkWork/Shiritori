2c36dbfae14047cf7d46ba6e1aa322eaed7b6692
Akinori Maesoko <s13012@std.it-college.ac.jp>
2015-08-16 22:42:27 +0900

    RoomFragmentにOnReceiveListenerを実装

--- app/src/main/java/jp/ac/it_college/std/shiritori/RoomFragment.java
+++ app/src/main/java/jp/ac/it_college/std/shiritori/RoomFragment.java
@@ -2,6 +2,7 @@ package jp.ac.it_college.std.shiritori;
 
 
 import android.app.ListFragment;
+import android.content.Intent;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,15 +10,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class RoomFragment extends ListFragment {
+public class RoomFragment extends ListFragment implements OnReceiveListener {
 
     private WifiP2pDevice device;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
         this.device = ((MainActivity) getActivity()).getDevice();
         ((TextView) getView().findViewById(R.id.my_name)).setText(device.deviceName);
+        ((MainActivity) getActivity()).getEventManager().addOnReceiveListener(this);
     }
 
     @Override
@@ -27,4 +30,23 @@ public class RoomFragment extends ListFragment {
         return inflater.inflate(R.layout.fragment_room, container, false);
     }
 
+    @Override
+    public void onStateChanged(Intent intent) {
+
+    }
+
+    @Override
+    public void onPeersChanged(Intent intent) {
+
+    }
+
+    @Override
+    public void onConnectionChanged(Intent intent) {
+
+    }
+
+    @Override
+    public void onDeviceChanged(Intent intent) {
+
+    }
 }

157416191cbd6c98288cf7613597bab776f7d5b0
Akinori Maesoko <s13012@std.it-college.ac.jp>
2015-08-16 19:50:20 +0900

    EventManagerクラスのイテレート処理をfor文に変更

--- app/src/main/java/jp/ac/it_college/std/shiritori/EventManager.java
+++ app/src/main/java/jp/ac/it_college/std/shiritori/EventManager.java
@@ -3,16 +3,11 @@ package jp.ac.it_college.std.shiritori;
 import android.content.Intent;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class EventManager implements OnReceiveListener {
     private List<OnReceiveListener> listeners = new ArrayList<>();
-    private Iterator<OnReceiveListener> iterator;
 
-    public EventManager() {
-        iterator = listeners.iterator();
-    }
 
     public void addOnReceiveListener(OnReceiveListener listener) {
         listeners.add(listener);
@@ -25,8 +20,8 @@ public class EventManager implements OnReceiveListener {
     /* WiFi Directの有効/無効状態が通知される。機能制限やユーザへの通知に利用 */
     @Override
     public void onStateChanged(Intent intent) {
-        while (iterator.hasNext()) {
-            iterator.next().onStateChanged(intent);
+        for (OnReceiveListener listener : listeners) {
+            listener.onStateChanged(intent);
         }
     }
 
@@ -34,24 +29,24 @@ public class EventManager implements OnReceiveListener {
     /* デバイス情報の変更通知（通信可能なデバイスの発見・ロストなど） */
     @Override
     public void onPeersChanged(Intent intent) {
-        while (iterator.hasNext()) {
-            iterator.next().onPeersChanged(intent);
+        for (OnReceiveListener listener : listeners) {
+            listener.onPeersChanged(intent);
         }
     }
 
     /* IPアドレスなどコネクション情報。通信状態の変更通知 */
     @Override
     public void onConnectionChanged(Intent intent) {
-        while (iterator.hasNext()) {
-            iterator.next().onConnectionChanged(intent);
+        for (OnReceiveListener listener : listeners) {
+            listener.onConnectionChanged(intent);
         }
     }
 
     /* 自分自身のデバイス状態の変更通知(相手デバイスではないことに注意) */
     @Override
     public void onDeviceChanged(Intent intent) {
-        while (iterator.hasNext()) {
-            iterator.next().onDeviceChanged(intent);
+        for (OnReceiveListener listener : listeners) {
+            listener.onDeviceChanged(intent);
         }
     }
 }

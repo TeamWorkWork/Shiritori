779c65e22dc60719b37c3d013c0398905d15ddc6
Akinori Maesoko <s13012@std.it-college.ac.jp>
2015-08-17 21:31:08 +0900

    MemberRoomFragmentの退室ボタン押下時の処理を実装

--- app/src/main/java/jp/ac/it_college/std/shiritori/MemberRoomFragment.java
+++ app/src/main/java/jp/ac/it_college/std/shiritori/MemberRoomFragment.java
@@ -1,13 +1,43 @@
 package jp.ac.it_college.std.shiritori;
 
 
+import android.app.ListFragment;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MemberRoomFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberRoomFragment extends ListFragment
+        implements OnReceiveListener, View.OnClickListener, DeviceActionListener {
+
+    private WifiP2pManager manager;
+    private WifiP2pManager.Channel channel;
+    private List<WifiP2pDevice> peers = new ArrayList<>();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ((MainActivity) getActivity()).getEventManager().addOnReceiveListener(this);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        getView().findViewById(R.id.btn_game_ready).setOnClickListener(this);
+        getView().findViewById(R.id.btn_room_exit).setOnClickListener(this);
+
+        setListAdapter(new WiFiPeerListAdapter(getActivity(), R.layout.row_devices, peers));
+        manager = ((MainActivity) getActivity()).getManager();
+        channel = ((MainActivity) getActivity()).getChannel();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -17,4 +47,77 @@ public class MemberRoomFragment extends Fragment {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_game_ready:
+                break;
+            case R.id.btn_room_exit:
+                onClickRoomExit();
+                break;
+        }
+    }
+
+    private void onClickRoomExit() {
+        disconnect();
+        getFragmentManager().beginTransaction()
+                .replace(R.id.container_root, new OpponentListFragment())
+                .commit();
+    }
+
+    /*
+    Implemented OnReceiveListener
+     */
+    @Override
+    public void onStateChanged(Intent intent) {
+
+    }
+
+    @Override
+    public void onPeersChanged(Intent intent) {
+
+    }
+
+    @Override
+    public void onConnectionChanged(Intent intent) {
+
+    }
+
+    @Override
+    public void onDeviceChanged(Intent intent) {
+
+    }
+
+    /*
+    Implemeted DeviceActionListener
+     */
+    @Override
+    public void cancelDisconnect() {
+
+    }
+
+    @Override
+    public void connect(WifiP2pConfig config) {
+
+    }
+
+    @Override
+    public void disconnect() {
+        manager.removeGroup(channel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+
+            }
+
+            @Override
+            public void onFailure(int reason) {
+
+            }
+        });
+    }
+
+    @Override
+    public void showDetails(WifiP2pDevice device) {
+
+    }
 }

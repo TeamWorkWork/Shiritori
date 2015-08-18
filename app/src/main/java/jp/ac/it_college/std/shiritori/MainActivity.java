ddec95498f1050b1f42fcd8991727056274c532b
s13012 <s13012@std.it-college.ac.jp>
2015-08-18 16:13:04 +0900

    ルーム画面の準備完了処理用の定数を追加

--- app/src/main/java/jp/ac/it_college/std/shiritori/MainActivity.java
+++ app/src/main/java/jp/ac/it_college/std/shiritori/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends Activity
     public static final int SERVER_PORT = 4545;
     public static final int MESSAGE_READ = 0x400 + 1;
     public static final int MY_HANDLE = 0x400 + 2;
+    public static final String GAME_READY = "GAME_READY_OK";
 
 
     @Override

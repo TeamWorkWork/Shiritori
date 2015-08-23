4bd7068df8d204feaa52ad6ff701904e1e028a85
Akinori Maesoko <s13012@std.it-college.ac.jp>
2015-08-24 01:05:28 +0900

    getDeviceStatusメソッドをWiFiPeerListAdapterクラスへ移動

--- app/src/main/java/jp/ac/it_college/std/shiritori/OpponentListFragment.java
+++ app/src/main/java/jp/ac/it_college/std/shiritori/OpponentListFragment.java
@@ -79,24 +79,6 @@ public class OpponentListFragment extends ListFragment
 
     }
 
-    private static String getDeviceStatus(int deviceStatus) {
-        switch (deviceStatus) {
-            case WifiP2pDevice.AVAILABLE:
-                return "Available";
-            case WifiP2pDevice.INVITED:
-                return "Invited";
-            case WifiP2pDevice.CONNECTED:
-                return "Connected";
-            case WifiP2pDevice.FAILED:
-                return "Failed";
-            case WifiP2pDevice.UNAVAILABLE:
-                return "Unavailable";
-            default:
-                return "Unknown";
-
-        }
-    }
-
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         OpponentDetailFragment fragment =

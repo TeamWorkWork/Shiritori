a452195d597bf57b9dfbcbdca739d5fef1d25818
Akinori Maesoko <s13012@std.it-college.ac.jp>
2015-08-24 16:12:26 +0900

    add RoomFragment

---
+++ app/src/main/java/jp/ac/it_college/std/shiritori/RoomFragment.java
@@ -0,0 +1,19 @@
+package jp.ac.it_college.std.shiritori;
+
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RoomFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+
+}
